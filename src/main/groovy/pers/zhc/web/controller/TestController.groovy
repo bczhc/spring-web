a287ee1344f39105af1b3df323083943660479c7
bczhc <bczhc0@126.com>
2021-05-09 17:56:05 +0800

    response

--- src/main/groovy/pers/zhc/web/controller/TestController.groovy
+++ src/main/groovy/pers/zhc/web/controller/TestController.groovy
@@ -1,23 +1,32 @@
 package pers.zhc.web.controller
 
+
 import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RestController
 
 import javax.servlet.http.HttpServletRequest
+import javax.servlet.http.HttpServletResponse
 
 @RestController
 class TestController {
     @Autowired
     private HttpServletRequest request
+    @Autowired
+    private HttpServletResponse response
 
-    @RequestMapping("/hello")
-    def hello() {
-        def is = request.getInputStream()
-        def buf = new byte[1024];
-        is.read(buf)
-        def s = Arrays.toString(buf)
-        println s
-        return s
+    @RequestMapping("/hello1")
+    def hello1() {
+        def headers = response.getHeader("Content-Type")
+        println headers
+        return [1, 2]
+    }
+
+    @GetMapping("/hello2")
+    def hello2() {
+        def headers = response.getHeader("Content-Type")
+        println headers
+        return ["a": 2]
     }
 }
